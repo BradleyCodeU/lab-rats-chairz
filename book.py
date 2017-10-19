4555a0df425f5fce0d5a901b57525ef224ad8118
sujaydammoju <32073316+sujaydammoju@users.noreply.github.com>
2017-10-19 13:39:08 -0400

    almost done with my class

--- book.py
+++ book.py
@@ -1,8 +1,19 @@
 class Book():
-    def __init__(self,name,color,):
-        self.name = Diary of a Wimpy Kid
-        self.color = Red
-       
+    # Constructor
+    def __init__(self,name,page,back,color):
+        self.name = "Diary of a Wimpy Kid"
+        self.page = "224"
+        self.back = "Paperback"
+        self.color = "Red"
+            
+    # Accesor
+    def getBack(self):
+        return self.back
+    def getColor(self):
+        return self.color
+
+    #Mutator
+    
 
         
 
