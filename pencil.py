c8cd95cd9dec9dabd7c4e2df216b9274a7d1f6eb
rayhanh <32073373+rayhanh@users.noreply.github.com>
2017-10-23 13:34:48 -0400

    new class

---
+++ pencil.py
@@ -0,0 +1,14 @@
+class pencil():
+    def init (self, color, sharpness)
+    self.color="yellow"
+    self.sharpness="sharp"
+
+    def getColor():
+        return self.color
+    def getSharpness():
+        return self.sharpness
+    def setColor(self newColor):
+        self.color=newColor
+    def setSharpness(self newSharpness):
+        self.sharpness=newSharpness
+        
