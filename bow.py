e8c014cd36be36e9aa8979958a0492c5f3684b56
Matthew Zilles <107486@hboe.org>
2017-10-23 13:18:08 -0400

    myclass #Belssed

--- bow.py
+++ bow.py
@@ -1,8 +1,22 @@
  class Bow():
-    def __init__(self, name,color,ammo)
-        self.name = Bow
-        self.color = Brown
+    #Constructor #BELSSED
+    def __init__(self, name,color,ammo,material)
+        self.name = "Bow"
+        self.color = "Brown"
+        self.material = "Wood"
         self.ammo = 0
         
+     #Accessors
+     def getColor (self);
+        return self.color
+     def getMaterial (self);
+        return self.material
+
+     #Mutators
+     def changeMaterial(self, newMaterial);
+        self.material = newMaterial
+     def changeColor (self, newColor);
+        self.color = newColor
+   
         
         
